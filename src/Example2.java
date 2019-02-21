5717308bf4456a07eba157f5308de7287ef1dc0b
unknown <cyhoyvanov@gmail.com>
2019-02-21 12:44:20 +0200

    first commit

---
+++ src/Example2.java
@@ -0,0 +1,11 @@
+
+public class Example2 {
+	 public static void main(String[] args){
+		    int q=21;
+		    int w=8;
+		    int a=q/w;
+		    int b=q%w;   
+		    System.out.println("21/8 равно " +a +" и "+b+ " в остатке.");
+		    }
+		}
+
